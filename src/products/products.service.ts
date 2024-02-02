358f8eabd0a7e1bbfbeebb340b904c61d6ab2cf2
Michel Santos <mmichel.santos@hotmail.com>
2024-02-01 22:45:23 -0300

    init

---
+++ src/products/products.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common'
+import { CreateProductDto } from './dto/create-product.dto'
+import { UpdateProductDto } from './dto/update-product.dto'
+import { Repository } from 'typeorm'
+import { Product } from './entities/product.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product) private productRepo: Repository<Product>,
+  ) {}
+
+  create(createProductDto: CreateProductDto) {
+    return 'This action adds a new product'
+  }
+
+  findAll() {
+    return this.productRepo.find()
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`
+  }
+}
